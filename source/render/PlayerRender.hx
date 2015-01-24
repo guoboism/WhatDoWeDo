5d0f2eae430dd4a159175f07f228fa25eb16d8dd
Sean <pixelpicosean@gmail.com>
2015-01-24 23:23:49 +0800

    Use left mouse button instead of "F" key to pick items

--- source/render/PlayerRender.hx
+++ source/render/PlayerRender.hx
@@ -1,9 +1,15 @@
 package render;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
+import flixel.util.FlxColor;
+import flixel.util.FlxPoint;
+import flixel.group.FlxTypedGroup;
 import scene.PlayState;
+
 import source.data.WDGame;
+import render.ItemRender;
 
 /**
  * 渲染玩家
@@ -11,19 +17,23 @@ import source.data.WDGame;
  */
 class PlayerRender extends FlxSprite {
 
-    private var ACCEL = 240;
-    private var MAX_SPEED = 70;
+    private var ACCEL:Int = 240;
+    private var MAX_SPEED:Int = 70;
+
+    // TODO: Use "sensor" to detect nearby items
+    private var _canReachItem:Bool = false;
+    private var _nearbyItem:ItemRender;
 
     public function new() {
         super(0, 0);
 
+        // Setup physics
         this.maxVelocity.set(MAX_SPEED, MAX_SPEED);
 
+        // Initialize graphics
         loadGraphic(AssetPaths.img_char__png);
         centerOrigin();
         centerOffsets();
-
-        trace("char loaded");
     }
 
     override public function update():Void {
@@ -53,13 +63,13 @@ class PlayerRender extends FlxSprite {
         }
 
         // "Handle" action
-        if (FlxG.keys.justPressed.F) {
-            trace("try to pick at (" + x +", " + y + ")");
-            //DO ACTIONS
-
-            //PICK UP item
-            var res:Bool = WDGame.getSelf().tryPickUpItem();
+        if (FlxG.mouse.justPressed) {
+            if (_canReachItem && _nearbyItem != null) {
+                this.pickItem(_nearbyItem);
+            }
         }
+        this._canReachItem = false;
+        this._nearbyItem = null;
 
         //update size with distance
         var distance = this.y - 32 * 6;
@@ -74,6 +84,21 @@ class PlayerRender extends FlxSprite {
         }
         this.scale.y = this.scale.x;
         this.centerOrigin();
+    }
+
+    override public function destroy():Void {
+        super.destroy();
+    }
+
+    public function touchesItem(item:ItemRender):Void {
+        this._canReachItem = true;
+        this._nearbyItem = item;
+    }
+
+    private function pickItem(item:ItemRender):Void {
+        WDGame.getSelf().pickUpItem(item.itemData);
+
+        trace("Picked " + WDGame.getSelf().bagItems.length + " items");
     }
 
 }
--- source/scene/PlayState.hx
+++ source/scene/PlayState.hx
@@ -5,6 +5,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.group.FlxGroup;
+import flixel.group.FlxTypedGroup;
 import flixel.input.touch.FlxTouchManager;
 import flixel.text.FlxText;
 import flixel.tile.FlxTilemap;
@@ -20,36 +21,28 @@ import ui.ItemCell;
 /**
  * A FlxState which can be used for the actual gameplay.
  */
-class PlayState extends FlxState
-{
-
-    var DISTENCE_GET_ITEM:Float = 50;
+class PlayState extends FlxState {
 
     public var player:PlayerRender;
-
+    private var _itemGroup:flixel.group.FlxTypedGroup<ItemRender>;
 
     public var uiLayer:FlxGroup;
 
     //scene stuff
     public var sceneImg:FlxSprite;
     public var sceneMap:FlxTilemap;
-    public var itemOnGroundRenders:Array<ItemRender>;
 
     //ui stuff
-    public var itemHinter:FlxSprite;
-    public var itemBgs:Array<ItemCell>;
-
 
     private static var self:PlayState;
-    public static function getSelf():PlayState{
+    public static function getSelf():PlayState {
         return self;
     }
 
     /**
      * Function that is called up when to state is created to set it up.
      */
-    override public function create():Void
-    {
+    override public function create():Void {
         super.create();
         self = this;
 
@@ -68,15 +61,14 @@ class PlayState extends FlxState
         add(sceneImg);
 
         //items on ground
-        itemOnGroundRenders = new Array<ItemRender>();
+        _itemGroup = new FlxTypedGroup<ItemRender>();
         for(one in WDGame.getSelf().listItemOnGround){
             var wdItem:WDItem = cast one;
             var itemRender:ItemRender = new ItemRender(wdItem);
             wdItem.linkedRender = itemRender;
-            itemOnGroundRenders.push(itemRender);
-            add(itemRender);
+            _itemGroup.add(itemRender);
         }
-
+        add(_itemGroup);
 
         //player stuff
         player = new PlayerRender();
@@ -88,93 +80,28 @@ class PlayState extends FlxState
         uiLayer = new FlxGroup();
         uiLayer.setAll("scrollFactor", FlxPoint.get(0, 0));
         add(uiLayer);
-
-        //UI things
-        //addItemCells
-        itemBgs = new Array<ItemCell>();
-        for (i in 0...WDGame.getSelf().MAX_BAG_ITEM) {
-            var itemCell:ItemCell = new ItemCell();
-            itemCell.setPos(  i * 60 + 450, 20  );
-            itemBgs.push(itemCell);
-            uiLayer.add(itemCell);
-        }
-
-        itemHinter = new FlxSprite();
-        itemHinter.loadGraphic(AssetPaths.img_hinter__png);
-        itemHinter.centerOffsets();
-        add(itemHinter);
     }
 
     /**
      * Function that is called when this state is destroyed - you might want to
      * consider setting all objects this state uses to null to help garbage collection.
      */
-    override public function destroy():Void
-    {
+    override public function destroy():Void {
         super.destroy();
     }
 
     /**
      * Function that is called once every frame.
      */
-    override public function update():Void
-    {
+    override public function update():Void {
         super.update();
 
+        FlxG.overlap(player, _itemGroup, touchesItem);
         FlxG.collide(player, sceneMap);
-
-        //check if near any items
-        // if (player.justMoved == true) {
-        //  checkNearItemOnGround();
-        //  player.justMoved = false;
-        // }
-
-        if (WDGame.getSelf().bagIitemDirty == true){
-            updateBagItem();
-            WDGame.getSelf().bagIitemDirty = false;
-        }
-
     }
 
-
-    public function checkNearItemOnGround():Void {
-
-        var nearAnyone:Bool = false;
-        WDGame.getSelf().selectedItemOnGround = null;
-        for(one in WDGame.getSelf().listItemOnGround){
-            var wdItem:WDItem = cast one;
-            var theRender:ItemRender = wdItem.linkedRender;
-            var distance:Float =  player.getMidpoint().distanceTo(theRender.getMidpoint());
-            if (distance < DISTENCE_GET_ITEM) {
-                itemHinter.x = theRender.x + (theRender.frameWidth - itemHinter.frameWidth)*0.5;
-                itemHinter.y = theRender.y - 35;
-                itemHinter.scale.x = itemHinter.scale.y = theRender.scale.x;
-                itemHinter.centerOrigin();
-                nearAnyone = true;
-                WDGame.getSelf().selectedItemOnGround = wdItem;
-                //trace(wdItem.name);
-                break;
-            }
-        }
-        itemHinter.visible = nearAnyone;
-    }
-
-
-    public function updateBagItem():Void{
-
-
-        for(i in 0...WDGame.getSelf().MAX_BAG_ITEM){
-
-            var exist:Bool =  i < WDGame.getSelf().bagItems.length;
-
-            var itemBg:ItemCell = itemBgs[i];
-            if(exist){
-                var wdItem:WDItem = WDGame.getSelf().bagItems[i];
-                itemBg.render(wdItem);
-            }else{
-                itemBg.render(null);
-            }
-        }
+    public function touchesItem(player:PlayerRender, item:ItemRender):Void {
+        player.touchesItem(item);
     }
 
 }
