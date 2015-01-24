b6ca41d67c510c0d51905113b6c70fc2494e3894
Sean <pixelpicosean@gmail.com>
2015-01-24 18:55:37 +0800

    Clean up some code

--- source/render/PlayerRender.hx
+++ source/render/PlayerRender.hx
@@ -1,4 +1,5 @@
 package render;
+
 import flixel.FlxG;
 import flixel.FlxSprite;
 import scene.PlayState;
@@ -6,71 +7,67 @@ import source.data.WDGame;
 
 /**
  * 渲染玩家
- * @author 
+ * @author
  */
-class PlayerRender extends FlxSprite
-{
-	
-	
-	public var SPEED = 100;
-	public var justMoved:Bool = false;
-	
-	public function new() 
-	{
-		super(0, 0);
-		loadGraphic(AssetPaths.img_char__png);
-		centerOrigin();
-		centerOffsets();
-		
-		trace("char loaded");
-	}
-	
-	override public function update():Void 
-	{
-		super.update();
-		
-		//check input
-		
-		if (FlxG.keys.pressed.W && !FlxG.keys.pressed.S) {
-			this.velocity.y = -SPEED;
-		} 
-		else if (!FlxG.keys.pressed.W && FlxG.keys.pressed.S) {
-			this.velocity.y = SPEED;
-		}
-		else {
-			this.velocity.y = 0;
-		}
-		
-		if (FlxG.keys.pressed.A && !FlxG.keys.pressed.D) {
-			this.velocity.x = -SPEED;
-		}
-		else if (!FlxG.keys.pressed.A && FlxG.keys.pressed.D) {
-			this.velocity.x = SPEED;
-		}
-		else {
-			this.velocity.x = 0;
-		}
-		
-		if (FlxG.keys.justPressed.F) {
-			trace("try to pick at (" + x +", " + y + ")");
-			//DO ACTIONS
-			
-			//PICK UP item
-			var res:Bool = WDGame.getSelf().tryPickUpItem();
-		}
-		
-		//update size with distance
-		var distance = this.y - 32 * 6;
-		var maxDistance = 32 * 10;
-		//this.scale.x = 0.5 *  distance * distance / (maxDistance * maxDistance) + 0.5;
-		this.scale.x = 0.5 *  Math.sqrt(distance )/ Math.sqrt(maxDistance) + 0.5;
-		if(this.scale.x  > 1){
-			this.scale.x  = 1;
-		}else if(this.scale.x  < 0.5){
-			this.scale.x  = 0.5;
-		}
-		this.scale.y = this.scale.x;
-		this.centerOrigin();
-	}
-	
+class PlayerRender extends FlxSprite {
+
+    public var SPEED = 70;
+    public var justMoved:Bool = false;
+
+    public function new() {
+        super(0, 0);
+        loadGraphic(AssetPaths.img_char__png);
+        centerOrigin();
+        centerOffsets();
+
+        trace("char loaded");
+    }
+
+    override public function update():Void {
+        super.update();
+
+        //check input
+        if (FlxG.keys.pressed.W && !FlxG.keys.pressed.S) {
+            this.velocity.y = -SPEED;
+        }
+        else if (!FlxG.keys.pressed.W && FlxG.keys.pressed.S) {
+            this.velocity.y = SPEED;
+        }
+        else {
+            this.velocity.y = 0;
+        }
+
+        if (FlxG.keys.pressed.A && !FlxG.keys.pressed.D) {
+            this.velocity.x = -SPEED;
+        }
+        else if (!FlxG.keys.pressed.A && FlxG.keys.pressed.D) {
+            this.velocity.x = SPEED;
+        }
+        else {
+            this.velocity.x = 0;
+        }
+
+        if (FlxG.keys.justPressed.F) {
+            trace("try to pick at (" + x +", " + y + ")");
+            //DO ACTIONS
+
+            //PICK UP item
+            var res:Bool = WDGame.getSelf().tryPickUpItem();
+        }
+
+        //update size with distance
+        var distance = this.y - 32 * 6;
+        var maxDistance = 32 * 10;
+        //this.scale.x = 0.5 *  distance * distance / (maxDistance * maxDistance) + 0.5;
+        this.scale.x = 0.5 *  Math.sqrt(distance) / Math.sqrt(maxDistance) + 0.5;
+        if (this.scale.x  > 1) {
+            this.scale.x  = 1;
+        }
+        else if (this.scale.x  < 0.5) {
+            this.scale.x  = 0.5;
+        }
+        this.scale.y = this.scale.x;
+        this.centerOrigin();
+    }
+
 }
--- source/scene/MenuState.hx
+++ source/scene/MenuState.hx
@@ -10,47 +10,37 @@ import flixel.util.FlxMath;
 /**
  * A FlxState which can be used for the game's menu.
  */
-class MenuState extends FlxState
-{
-	
-	
-	
-	
-	
-	private var _playButton:FlxButton;
-	
-	
-	
-	/**
-	 * Function that is called up when to state is created to set it up.
-	 */
-	override public function create():Void
-	{
-		super.create();
-		
-		_playButton = new FlxButton(300, 400, "CLICK HERE", onPlay);
-		add(_playButton);
-		
-	}
+class MenuState extends FlxState {
 
-	/**
-	 * Function that is called when this state is destroyed - you might want to
-	 * consider setting all objects this state uses to null to help garbage collection.
-	 */
-	override public function destroy():Void
-	{
-		super.destroy();
-	}
-	
-	private function onPlay():Void{
-		FlxG.switchState(new PlayState());
-	}
+    private var _playButton:FlxButton;
 
-	/**
-	 * Function that is called once every frame.
-	 */
-	override public function update():Void
-	{
-		super.update();
-	}
+    /**
+     * Function that is called up when to state is created to set it up.
+     */
+    override public function create():Void {
+        super.create();
+
+        _playButton = new FlxButton(FlxG.width / 2 - 80 / 2, FlxG.height / 2 - 20 / 2, "Enjoy", onPlay);
+        add(_playButton);
+    }
+
+    /**
+     * Function that is called when this state is destroyed - you might want to
+     * consider setting all objects this state uses to null to help garbage collection.
+     */
+    override public function destroy():Void {
+        super.destroy();
+    }
+
+    private function onPlay():Void {
+        FlxG.switchState(new PlayState());
+    }
+
+    /**
+     * Function that is called once every frame.
+     */
+    override public function update():Void {
+        super.update();
+    }
+
 }
